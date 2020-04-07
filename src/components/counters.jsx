3a0ade179d4909e7435ffe2347dec7e320573970
PrachiSalvi08 <prachisalvi08@gmail.com>
2020-04-07 09:59:30 +0530

    Add Reset Button with handleReset Function

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,12 +4,20 @@ import Counter from './counter';
 class Counters extends Component {
     state = { 
         counters : [
-            { id: 1, value: 4 },
+            { id: 1, value: 0 },
             { id: 2, value: 0 },
             { id: 3, value: 0 },
             { id: 4, value: 0 }
         ]
      };
+
+     handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({counters});
+     }
      handleDelete = counterID => {
        // console.log("Event Handeler called", counterID );
         const counters = this.state.counters.filter(c => c.id !== counterID );
@@ -19,6 +27,7 @@ class Counters extends Component {
     render() { 
         return ( 
             <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
                 {this.state.counters.map(counter => 
                 <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}>
                     <h4>Counter #{counter.id}</h4>   
