80805c550b0e782ca0a43c631d34304193b8251f
PrachiSalvi08 <prachisalvi08@gmail.com>
2020-04-07 12:02:18 +0530

    Stateless Functional Components and Destructing Argument

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,11 +3,12 @@ import Counter from './counter';
 
 class Counters extends Component {
     render() { 
+        const {onReset, counters, onIncrement, onDelete} = this.props;
         return ( 
             <div>
-                <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
-                {this.props.counters.map(counter => 
-                <Counter key={counter.id} onIncrement={this.props.onIncrement} onDelete={this.props.onDelete} counter={counter}>
+                <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+                {counters.map(counter => 
+                <Counter key={counter.id} onIncrement={onIncrement} onDelete={onDelete} counter={counter}>
                     <h4>Counter #{counter.id}</h4>   
                 </Counter>
                  )}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 
 class NavBar extends Component {
     render() { 
+        const {totalCounters} = this.props;
         return ( 
             <nav className="navbar navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar <span className="span.badge.badge-pill.badge-secondary">{this.props.totalCounters}</span></a>
+                <a className="navbar-brand" href="#">Navbar <span className="span.badge.badge-pill.badge-secondary">{totalCounters}</span></a>
             </nav>
         );
     }
 }
+
+//Stateless Functional Components. (No render Method)
+/*const NavBar = (props) => {
+    return ( 
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar <span className="span.badge.badge-pill.badge-secondary">{props.totalCounters}</span></a>
+        </nav>
+     );
+}
  
+export default NavBar;*/
+
+
 export default NavBar;
